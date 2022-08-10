bf554d5ae82d677836b3984a9b2393ee9cf21771
Karen Munoz <karen00munoz@gmail.com>
2022-08-10 13:10:18 +0200

    Add CineDialog

--- src/App.js
+++ src/App.js
@@ -1,11 +1,216 @@
-import { LoadImage } from './components/ImageLoader';
+import React, { useState } from "react";
+import * as cornerstone from "cornerstone-core";
+import * as cornerstoneMath from "cornerstone-math";
+import * as cornerstoneTools from "cornerstone-tools";
+import Hammer from "hammerjs";
+import * as cornerstoneWebImageLoader from "cornerstone-web-image-loader";
+import { CineDialog } from 'react-viewerbase'
+// import { ConnectedCineDialog } from "./components/Cine";
 
-function App() {
-  return (
-    <div className="App">
-      <LoadImage />
-    </div>
-  );
+cornerstoneTools.external.cornerstone = cornerstone;
+cornerstoneTools.external.cornerstoneMath = cornerstoneMath;
+cornerstoneWebImageLoader.external.cornerstone = cornerstone;
+cornerstoneTools.external.Hammer = Hammer;
+
+const imageId =
+  "https://rawgit.com/cornerstonejs/cornerstoneWebImageLoader/master/examples/Renal_Cell_Carcinoma.jpg";
+
+const divStyle = {
+  width: "512px",
+  height: "512px",
+  position: "relative",
+  color: "white"
+};
+
+const bottomLeftStyle = {
+  bottom: "5px",
+  left: "5px",
+  position: "absolute",
+  color: "white"
+};
+
+const bottomRightStyle = {
+  bottom: "5px",
+  right: "5px",
+  position: "absolute",
+  color: "white"
+};
+
+class CornerstoneElement extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      stack: props.stack,
+      viewport: cornerstone.getDefaultViewport(null, undefined),
+      imageId: props.stack.imageIds[0],
+    };
+
+    this.onImageRendered = this.onImageRendered.bind(this);
+    this.onNewImage = this.onNewImage.bind(this);
+    this.onWindowResize = this.onWindowResize.bind(this);
+  }
+
+  render() {
+    return (
+      <div>
+        <div
+          className="viewportElement"
+          style={divStyle}
+          ref={input => {
+            this.element = input;
+          }}
+        >
+          <canvas className="cornerstone-canvas" />
+          <div style={bottomLeftStyle}>Zoom: {this.state.viewport.scale}</div>
+          <div style={bottomRightStyle}>
+            WW/WC: {this.state.viewport.voi.windowWidth} /{" "}
+            {this.state.viewport.voi.windowCenter}
+          </div>
+        </div>
+      </div>
+    );
+  }
+
+  onWindowResize() {
+    console.log("onWindowResize");
+    cornerstone.resize(this.element);
+  }
+
+  onImageRendered() {
+    const viewport = cornerstone.getViewport(this.element);
+    console.log(viewport);
+
+    this.setState({
+      viewport
+    });
+
+    console.log(this.state.viewport);
+  }
+
+  onNewImage() {
+    const enabledElement = cornerstone.getEnabledElement(this.element);
+
+    this.setState({
+      imageId: enabledElement.image.imageId
+    });
+  }
+
+  componentDidMount() {
+    const element = this.element;
+
+    // Enable the DOM Element for use with Cornerstone
+    cornerstone.enable(element);
+
+    // Load the first image in the stack
+    cornerstone.loadImage(this.state.imageId).then(image => {
+      // Display the first image
+      cornerstone.displayImage(element, image);
+
+      // Add the stack tool state to the enabled element
+      const stack = this.props.stack;
+      cornerstoneTools.addStackStateManager(element, ["stack"]);
+      cornerstoneTools.addToolState(element, "stack", stack);
+
+      cornerstoneTools.mouseInput.enable(element);
+      cornerstoneTools.mouseWheelInput.enable(element);
+      cornerstoneTools.wwwc.activate(element, 1); // ww/wc is the default tool for left mouse button
+      cornerstoneTools.pan.activate(element, 2); // pan is the default tool for middle mouse button
+      cornerstoneTools.zoom.activate(element, 4); // zoom is the default tool for right mouse button
+      cornerstoneTools.zoomWheel.activate(element); // zoom is the default tool for middle mouse wheel
+
+      cornerstoneTools.touchInput.enable(element);
+      cornerstoneTools.panTouchDrag.activate(element);
+      cornerstoneTools.zoomTouchPinch.activate(element);
+      cornerstoneTools.playClip(this.element, 1);
+      cornerstoneTools.stopClip(this.element);
+
+      element.addEventListener(
+        "cornerstoneimagerendered",
+        this.onImageRendered
+      );
+      element.addEventListener("cornerstonenewimage", this.onNewImage);
+      window.addEventListener("resize", this.onWindowResize);
+    });
+  }
+
+  componentWillUnmount() {
+    const element = this.element;
+    element.removeEventListener(
+      "cornerstoneimagerendered",
+      this.onImageRendered
+    );
+
+    element.removeEventListener("cornerstonenewimage", this.onNewImage);
+
+    window.removeEventListener("resize", this.onWindowResize);
+
+    cornerstone.disable(element);
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    const stackData = cornerstoneTools.getToolState(this.element, "stack");
+    const stack = stackData.data[0];
+    stack.currentImageIdIndex = this.state.stack.currentImageIdIndex;
+    stack.imageIds = this.state.stack.imageIds;
+    cornerstoneTools.addToolState(this.element, "stack", stack);
+
+    //const imageId = stack.imageIds[stack.currentImageIdIndex];
+    //cornerstoneTools.scrollToIndex(this.element, stack.currentImageIdIndex);
+  }
 }
 
-export default App;
+const stack = {
+  imageIds: [imageId, 'https://images.unsplash.com/photo-1494976388531-d1058494cdd8?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxleHBsb3JlLWZlZWR8M3x8fGVufDB8fHx8&w=1000&q=80'],
+  currentImageIdIndex: 0
+};
+
+const App = () => {
+  setTimeout(() => {
+  })
+  let element;
+  const [isPlaying, setIsPlaying] = useState(false);
+    element = document.querySelectorAll(".viewportElement");
+    console.log(element);
+    for (let i = 0; i < element.length; i++) {
+      cornerstone.enable(element[i]);
+    }
+  const setPlayClip = (e) => {
+    for (let i = 0; i < element.length; i++) {
+      cornerstoneTools.playClip(element, 1);
+      setIsPlaying(true);
+    }
+    console.log(element);
+  };
+
+  const setStopClip = (e) => {
+    for (let i = 0; i < element.length; i++) {
+      cornerstoneTools.stopClip(element);
+      setIsPlaying(false);
+    }
+  };
+  return (
+    <div>
+      <h2>Cornerstone React Component Example</h2>
+      <button onClick={() => setPlayClip()} style={{ marginLeft: "10px" }}>
+        Play
+      </button>
+      <button onClick={() => setStopClip()} style={{ marginLeft: "10px" }}>
+        Stop
+      </button>
+      <CineDialog
+        isPlaying={isPlaying}
+        onClickSkipToStart={() => console.log("skip pressed")}
+        onClickSkipToEnd={() => console.log("skip to end pressed")}
+        onClickNextButton={() => console.log("next pressed")}
+        onClickBackButton={() => console.log("back pressed")}
+        onLoopChanged={(value) => console.log("loop changed")}
+        onFrameRateChanged={(value) => console.log("frame rate changed")}
+        onPlayPauseChanged={() => console.log("play pause pressed")}
+      />
+      <CornerstoneElement stack={{ ...stack }} />
+    </div>
+  );
+};
+
+  export default App;
+  
